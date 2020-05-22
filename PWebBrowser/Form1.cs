818b6e01f261e7aa4a2f93e65af906e39beb6616
Fedor Zhilkin <fzhilkin@mail.ru>
2020-05-22 21:53:15 +0300

    Добавьте файлы проекта.

---
+++ PWebBrowser/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PWebBrowser
+{
+    public partial class Form1 : Form
+    {
+        private string strINIFile = "browser.ini";
+        private const string HeadWindow = "[Window]";
+        private const string HeadBrowser = "[Browser]";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Обработчик события закрытия формы
+        private void Form1_FormClosed(Object sender, FormClosedEventArgs e)
+        {
+            
+            // Здесь нужно написать текст функции!
+           
+        }
+
+        // Обработчик события загрузки формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Здесь нужно написать текст функции!
+        }
+
+        
+        // Обработчик события нажатия клавиши
+        private void txtURL_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            // Check for the flag being set in the KeyDown event.
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                //MessageBox.Show(txtURL.Text);
+                wbBrowser.Navigate(txtURL.Text);
+                e.Handled = true;
+            }
+
+        }
+
+        
+    }
+}
